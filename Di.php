91d7e781ae4c160f2f9fc9f52f5e9c5d4f1c723d
surikat <jo@surikat.pro>
2016-06-10 16:03:10 +0200

    add the + to merge

--- Di.php
+++ Di.php
@@ -12,7 +12,7 @@
  *		full registry implementation, freeze optimisation
  * 
  * @package Ding
- * @version 3.5.0
+ * @version 3.6.0
  * @link http://github.com/redcatphp/Ding/
  * @author Jo Surikat <jo@surikat.pro>
  * @website http://redcatphp.com
@@ -528,21 +528,30 @@ class Di implements \ArrayAccess{
 			$this->recursiveResolveVar($php['$']);
 			foreach($php['$'] as $key=>$value){
 				$fc = substr($key,0,1);
-				if($fc==':'){
-					$key = substr($key,1);
-					if(isset($this[$key])&&is_array($this[$key])){
-						$this[$key] = self::merge_recursive($this[$key],$value);
-						continue;
-					}
-				}
-				elseif($fc=='!'){
-					$key = substr($key,1);
-					if(isset($this[$key])){
-						if(is_array($this[$key])){
-							$this[$key] = self::merge_recursive($value,$this[$key]);
+				switch($fc){
+					case ':':
+						$key = substr($key,1);
+						if(isset($this[$key])&&is_array($this[$key])){
+							$this[$key] = self::merge_recursive($this[$key],$value);
+							continue 2;
 						}
-						continue;
-					}
+					break;
+					case '+':
+						$key = substr($key,1);
+						if(isset($this[$key])&&is_array($this[$key])){
+							$this[$key] = array_merge($this[$key],$value);
+							continue 2;
+						}
+					break;
+					case '!':
+						$key = substr($key,1);
+						if(isset($this[$key])){
+							if(is_array($this[$key])){
+								$this[$key] = self::merge_recursive($value,$this[$key]);
+							}
+							continue 2;
+						}
+					break;
 				}
 				$this[$key] = $value;
 			}
