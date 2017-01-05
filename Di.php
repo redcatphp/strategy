303099b056ee63e006217ef7a0b5a9fea57551aa
jo <jo@surikat.pro>
2017-01-05 09:36:55 +0100

    update version

--- Di.php
+++ Di.php
@@ -9,7 +9,7 @@
  *		full registry implementation
  * 
  * @package Strategy
- * @version 5.1.0
+ * @version 5.1.1
  * @link http://github.com/redcatphp/Strategy/
  * @author Jo Surikat <jo@surikat.pro>
  * @website http://redcatphp.com
